91c7f155104392e5fa968c0e8c3a76f84657296d
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-03-13 23:39:35 +0100

    Add configs to link cy and vite

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        host: true,
+        port: 3000,
+    },
     envDir: './environment',
     base: '/tv-app',
     resolve: {
