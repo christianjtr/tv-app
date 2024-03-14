796c9f5f73cd179e6f37ac8f0e3783862b404ba6
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-03-14 03:36:43 +0100

    Add scrollTo effect

--- app/src/routes/AppRoutes.tsx
+++ app/src/routes/AppRoutes.tsx
@@ -1,6 +1,6 @@
-import React, { lazy } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import { APP_BASE_PATH } from '@config/app.config';
+import React, { lazy, useEffect } from 'react';
+import { Route, Switch, Redirect, useLocation } from 'react-router-dom';
+import { APP_BASE_PATH } from '@configuration/app.config';
 
 const Home = lazy(() => import('@pages/Home'));
 const Detail = lazy(() => import('@pages/Detail/Detail'));
@@ -8,6 +8,12 @@ const Search = lazy(() => import('@pages/Search'));
 const NotFound = lazy(() => import('@pages/NotFound'));
 
 const AppRoutes: React.FC = (): React.ReactElement => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
     return (
         <Switch>
             <Redirect exact from={APP_BASE_PATH} to="/" />
