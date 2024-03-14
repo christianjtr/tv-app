af790004da4755bcf2a2113ea1632550f5b09fe7
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-03-14 15:41:00 +0100

    Enhance season card

--- app/src/components/SeasonCard.tsx
+++ app/src/components/SeasonCard.tsx
@@ -14,13 +14,17 @@ const SeasonCard: React.FC<SeasonCardProps> = (props: SeasonCardProps) => {
     const formattedReleaseDate = new Date(releaseDate!).toLocaleDateString('en-US', { month: 'long', year: 'numeric' });
 
     return (
-        <div className="card bg-base-100 shadow-lg" data-element="season-card" tabIndex={0}>
+        <div
+            className="card bg-base-200 shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-base-300 duration-300"
+            data-element="season-card"
+            tabIndex={0}
+        >
             <figure>
-                <img src={info.images.w185} alt="Season poster" aria-label="Season poster" />
+                <img src={info.images.w300} alt="Season poster" aria-label="Season poster" className="rounded-t-md md:w-full sm:w-fit xs:w-fit" />
             </figure>
             <div className="card-body text-base sm:flex xs:flex items-center">
                 <h3 className="card-title">{info.name}</h3>
-                <Rating value={info.rating} />
+                <Rating value={info.rating} className="border-none" />
                 <p>
                     <span className="block">{formattedReleaseDate}</span>
                     <span className="block">Season: {info.seasonNumber}</span>
