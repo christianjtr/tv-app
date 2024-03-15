5f1c944bf81378147da3d7745e0d9e96f93db9bf
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-03-15 04:05:27 +0100

    Add a few other cofigs and dependencies

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const mode = process.env.NODE_ENV;
+const mode = process.env.NODE_ENV || 'development';
 
 // https://vitejs.dev/config/
 export default defineConfig({
