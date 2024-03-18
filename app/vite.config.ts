ed0f685ab9111b860de6ca54ab6dc06d1f82f065
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-03-18 16:59:42 +0100

    Fix vite build for production

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,31 +1,48 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const mode = process.env.NODE_ENV || 'development';
-const isProduction = process.env.NODE_ENV === 'production';
-
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    mode,
-    server: {
-        host: true,
-        port: +process.env.VITE_APP_SERVER_PORT! || 3000,
-    },
-    envDir: './environment',
-    base: isProduction ? '/' : '/tv-app',
-    resolve: {
-        alias: [
-            { find: '@services', replacement: resolve(__dirname, './src/services') },
-            { find: '@hooks', replacement: resolve(__dirname, './src/hooks') },
-            { find: '@pages', replacement: resolve(__dirname, './src/pages') },
-            { find: '@configuration', replacement: resolve(__dirname, './src/configuration') },
-            { find: '@app-types', replacement: resolve(__dirname, './src/types') },
-            { find: '@components', replacement: resolve(__dirname, './src/components') },
-            { find: '@assets', replacement: resolve(__dirname, './src/assets') },
-            { find: '@layout', replacement: resolve(__dirname, './src/layout') },
-            { find: '@routes', replacement: resolve(__dirname, './src/routes') },
+export default defineConfig(({ mode }) => {
+    const isProduction = mode === 'production';
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [
+            react({
+                babel: {
+                    plugins: [['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]],
+                },
+            }),
         ],
-    },
+        mode,
+        server: {
+            host: true,
+            port: +JSON.stringify(env.VITE_APP_SERVER_PORT)! || 3000,
+        },
+        define: {
+            ...Object.keys(env).reduce(
+                (prev, key): Record<PropertyKey, string> => {
+                    prev[`process.env.${key}`] = JSON.stringify(env[key]);
+                    return prev;
+                },
+                {} as Record<PropertyKey, string>
+            ),
+        },
+        envDir: './environment',
+        base: isProduction ? '/' : '/tv-app',
+        resolve: {
+            alias: [
+                { find: '@services', replacement: resolve(__dirname, './src/services') },
+                { find: '@hooks', replacement: resolve(__dirname, './src/hooks') },
+                { find: '@pages', replacement: resolve(__dirname, './src/pages') },
+                { find: '@configuration', replacement: resolve(__dirname, './src/configuration') },
+                { find: '@app-types', replacement: resolve(__dirname, './src/types') },
+                { find: '@components', replacement: resolve(__dirname, './src/components') },
+                { find: '@assets', replacement: resolve(__dirname, './src/assets') },
+                { find: '@layout', replacement: resolve(__dirname, './src/layout') },
+                { find: '@routes', replacement: resolve(__dirname, './src/routes') },
+            ],
+        },
+    };
 });
